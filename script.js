8f64a032b08f16954f4063ff53e98ebd9fc8b369
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-03-08 20:07:41 +0600

    fix equl er pore number display te add hoye jawa

--- script.js
+++ script.js
@@ -2,61 +2,72 @@
 document.getElementById("one").addEventListener("click", () => {
 
     const display = document.getElementById("display");
-    display.innerText = display.innerText == 0 ? 1 : display.innerText += 1;
+    const isIncludeEqual = display.innerText.includes("=");
+    display.innerText = display.innerText == 0 ? 1 : isIncludeEqual ? display.innerText : display.innerText += 1;
+
 })
 
 document.getElementById("two").addEventListener("click", () => {
 
     const display = document.getElementById("display");
-    display.innerText = display.innerText == 0 ? 2 : display.innerText += 2;
+    const isIncludeEqual = display.innerText.includes("=");
+    display.innerText = display.innerText == 0 ? 2 : isIncludeEqual ? display.innerText : display.innerText += 2;
 })
 
 document.getElementById("three").addEventListener("click", () => {
 
     const display = document.getElementById("display");
-    display.innerText = display.innerText == 0 ? 3 : display.innerText += 3;
+    const isIncludeEqual = display.innerText.includes("=");
+    display.innerText = display.innerText == 0 ? 3 : isIncludeEqual ? display.innerText : display.innerText += 3;
 })
 
 document.getElementById("four").addEventListener("click", () => {
 
     const display = document.getElementById("display");
-    display.innerText = display.innerText == 0 ? 4 : display.innerText += 4;
+    const isIncludeEqual = display.innerText.includes("=");
+    display.innerText = display.innerText == 0 ? 4 : isIncludeEqual ? display.innerText : display.innerText += 4;
 })
 
 document.getElementById("five").addEventListener("click", () => {
 
     const display = document.getElementById("display");
-    display.innerText = display.innerText == 0 ? 5 : display.innerText += 5;
+    const isIncludeEqual = display.innerText.includes("=");
+    display.innerText = display.innerText == 0 ? 5 : isIncludeEqual ? display.innerText : display.innerText += 5;
 })
 
 document.getElementById("six").addEventListener("click", () => {
 
     const display = document.getElementById("display");
-    display.innerText = display.innerText == 0 ? 6 : display.innerText += 6;
+    const isIncludeEqual = display.innerText.includes("=");
+    display.innerText = display.innerText == 0 ? 6 : isIncludeEqual ? display.innerText : display.innerText += 6;
 })
 
 document.getElementById("seven").addEventListener("click", () => {
 
     const display = document.getElementById("display");
-    display.innerText = display.innerText == 0 ? 7 : display.innerText += 7;
+    const isIncludeEqual = display.innerText.includes("=");
+    display.innerText = display.innerText == 0 ? 7 : isIncludeEqual ? display.innerText : display.innerText += 7;
 })
 
 document.getElementById("eight").addEventListener("click", () => {
 
     const display = document.getElementById("display");
-    display.innerText = display.innerText == 0 ? 8 : display.innerText += 8;
+    const isIncludeEqual = display.innerText.includes("=");
+    display.innerText = display.innerText == 0 ? 8 : isIncludeEqual ? display.innerText : display.innerText += 8;
 })
 
 document.getElementById("nine").addEventListener("click", () => {
 
     const display = document.getElementById("display");
-    display.innerText = display.innerText == 0 ? 9 : display.innerText += 9;
+    const isIncludeEqual = display.innerText.includes("=");
+    display.innerText = display.innerText == 0 ? 9 : isIncludeEqual ? display.innerText : display.innerText += 9;
 })
 
 document.getElementById("zero").addEventListener("click", () => {
 
     const display = document.getElementById("display");
-    display.innerText = display.innerText == 0 ? 0 : display.innerText += 0;
+    const isIncludeEqual = display.innerText.includes("=");
+    display.innerText = display.innerText == 0 ? 0 : isIncludeEqual ? display.innerText : display.innerText += 0;
 })
 
 document.getElementById("clear").addEventListener("click", () => {
@@ -102,8 +113,6 @@ document.getElementById("calculate").addEventListener("click", () => {
     const regex = /['+'-*/]/ig;
     const position = display.innerText.search(regex);
     const operator = display.innerText.match(regex);
-    // console.log(position, "Position");
-    // console.log(operator, "operator");
 
     if(position == -1 && operator == null){
         return;
